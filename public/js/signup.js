2e117e3e9b33c97f61c998bc229255b6fcda5ed3
naomi <naomilounsbury@hotmail.com>
2022-02-14 14:58:17 -0800

    debugging, fixing the login and signup, just drowning basically, I'm stuck on the create post.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,12 +1,13 @@
+console.log("SIGNUPFORMBEGINNING")
 async function signupFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  
+  console.log({username, email, password})
     if (username && email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/signup', {
         method: 'post',
         body: JSON.stringify({
           username,
@@ -15,7 +16,8 @@ async function signupFormHandler(event) {
         }),
         headers: { 'Content-Type': 'application/json' }
       });
-  
+  console.log(response)
+  console.log("SIGNUP RESPONSE")
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
@@ -24,4 +26,4 @@ async function signupFormHandler(event) {
     }
   }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
